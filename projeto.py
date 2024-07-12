6962ed484281fd107a3cf4f55e1a9646b1857964
Yukiii32 <diogoteixeira795@gmail.com>
2024-07-12 13:33:56 -0300

    projeto2.py

--- projeto.py
+++ projeto.py
@@ -6,21 +6,29 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from fpdf import FPDF
 
 def scrape_kabum(url="https://www.kabum.com.br/hardware/processadores/intel"):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     )
+
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=options
     )
     driver.get(url)
 
     WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located(("css selector", "article"))
+        EC.presence_of_element_located((By.CSS_SELECTOR, "article"))
     )
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -43,31 +51,31 @@ def create_pdf(df):
     pdf.set_font("Arial", "B", 12)
     pdf.cell(0, 10, "KABUM PROCESSADORES INTEL", 0, 1, "C")
 
-    col_widths = (168, 25)  
+    col_widths = (168, 25)
 
     # Headers
     for col, width in zip(df.columns, col_widths):
         pdf.cell(width, 10, col, 1, 0, "C")
     pdf.ln()
 
-    pdf.set_font("Arial", size=7) 
+    pdf.set_font("Arial", size=7)
     for _, row in df.iterrows():
-        pdf.cell(col_widths[0], 8, row['Nome'], 1, 0)  
-        pdf.cell(col_widths[1], 8, row['Preço'], 1, 1)  
+        pdf.cell(col_widths[0], 8, row['Nome'], 1, 0)
+        pdf.cell(col_widths[1], 8, row['Preço'], 1, 1)
 
     return pdf.output(dest="S").encode("latin-1")
 
 st.title("TRABALHO AVALIATIVO 07")
-st.empty()  
-st.markdown(" #   Diogo Dutra Teixeira") 
+st.empty()
+st.markdown(" #   Diogo Dutra Teixeira")
 
 if st.button("PEGAR INFORMAÇÕES"):
     with st.empty():
-     with st.spinner("CARREGANDO..."):
-        df = scrape_kabum()
+        with st.spinner("CARREGANDO..."):
+            df = scrape_kabum()
 
     st.subheader("KABUM RESULTADOS:")
-    st.dataframe(df, use_container_width=True)  
+    st.dataframe(df, use_container_width=True)
 
     if not df.empty:
         st.download_button(
